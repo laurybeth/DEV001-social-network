a90b4e9a4126546864d9d7ccf593d65603e5384f
laurybeth <laurybeth@msn.com>
2022-11-15 17:24:35 -0500

    instalación de firebase y se solucionó la  dependencia circular

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,21 +1,19 @@
-import { onNavigate } from "../main.js";
+export const Welcome = (onNavigate) => {
+  const $div = document.createElement('div');
+  const $title = document.createElement('h2');
+  const $buttonLogin = document.createElement('button');
+  const $buttonRegister = document.createElement('button');
 
-export const Welcome = () => {
-  const $div = document.createElement("div");
-  const $title = document.createElement("h2");
-  const $buttonLogin = document.createElement("button");
-  const $buttonRegister = document.createElement("button");
-
-  $title.textContent = "Welcome to PetBook";
-  $buttonLogin.textContent = "Login";
-  $buttonRegister.textContent = "Sign In";
-  $buttonLogin.addEventListener("click", () => {
-    onNavigate("/login");
+  $title.textContent = 'Welcome to PetBook =D';
+  $buttonLogin.textContent = 'Login';
+  $buttonRegister.textContent = 'Sign Up';
+  $buttonLogin.addEventListener('click', () => {
+    onNavigate('/login');
   });
 
-  $buttonRegister.addEventListener("click", () => {
-    onNavigate("/Register");
-    console.log("Register");
+  $buttonRegister.addEventListener('click', () => {
+    onNavigate('/Register');
+    console.log('Register');
   });
 
   $div.append($title, $buttonRegister, $buttonLogin);
