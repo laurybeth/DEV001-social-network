a159c38d45b5a71702d456f79b99a31f5c76639d
laurybeth <laurybeth@msn.com>
2022-11-25 18:58:09 -0500

    se creó archivo  Register.spec.js

--- src/lib_firebase/auth.js
+++ src/lib_firebase/auth.js
@@ -1,17 +1,15 @@
 import { createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { auth } from '../firebase';
 
-
 /**
- 
+
  */
 export const registerFunction = (email, password) => createUserWithEmailAndPassword(auth, email, password);
 
-export const registerGoogleFunction = () => 
-{
+export const registerGoogleFunction = () => {
   const provider = new GoogleAuthProvider();
-  //const credential = provider.credentialFromResult(result);
-  //const token = credential.accessToken;
+  // const credential = provider.credentialFromResult(result);
+  // const token = credential.accessToken;
 
   return signInWithPopup(auth, provider);
-}
+};
