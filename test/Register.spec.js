0a5459585fe260d1ffb3b5bebab3e9843f6e0641
JENNYFERGAMBOA <jennyfergamboaaponte@gmail.com>
2022-11-28 11:57:04 -0500

    test a submit; si al dar submit al botó este envia un formulario

--- test/Register.spec.js
+++ test/Register.spec.js
@@ -1,9 +1,14 @@
-import {Register} from '../src/components/Register.js';
+import { Register } from '../src/components/Register.js';
 
-describe ('Test a botón de registro', ()=>{
- it ("Probar si existe el botón registro", ()=>{
-    const Registro = Register();
-    const button = Registro.querySelector('.container__button__signup')
+describe('Test to register', () => {
+  const element = Register();
+  const button = element.querySelector('.container__button__signup');
+  const form = element.querySelector('.container__form');
+  it('Probar si existe el botón registro', () => {
     expect(button).not.toBeNull();
-    });
+  });
+  it('Probar si al dar submit al botón de registro este envía un formulario', () => {
+    button.dispatchEvent(new Event('submit'));
+    expect(form).not.toBeNull();
+  });
 });
