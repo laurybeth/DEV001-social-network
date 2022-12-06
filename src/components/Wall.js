847ff3bea4c2f6d54d12edc32ed99cc5e202381b
laurybeth <laurybeth@msn.com>
2022-12-06 15:41:20 -0500

    estilos a elementos del wall

--- src/components/Wall.js
+++ src/components/Wall.js
@@ -1,5 +1,6 @@
 /* import {wallFunction} from '../lib_firebase/auth'; */
-/*import { querySnapshot } from '../lib_firebase/db.js';*/
+/* import { querySnapshot } from '../lib_firebase/db.js'; */
+import { Modal } from './Modal.js';
 
 export const Wall = (onNavigate) => {
   const $section = document.createElement('section');
@@ -13,12 +14,12 @@ export const Wall = (onNavigate) => {
         <div class="container-imgProfile">
             <img class="container-imgProfile__img" src="./assets/img/user_photo_default.png">
         </div>
-        <div class="container-addPost__text" rows="5" placeholder="What are you thinking, Yin Yang?"></div>
-        <img class="container-addPost__add" src="./assets/img/icon_addImage.png">
-    </section>
+        <div class="container-addPost__text" id="addPost" >What are you thinking, Yin Yang?</div>
+   </section>
 
-    <section class='viewPost'>
-        <div>
+   <section class='container-Posts'>
+    <div class='container-Posts__Post'>
+        <div class="container-headerPost">
             <img src="">
             <label></label>
             <img src="">
@@ -49,12 +50,20 @@ export const Wall = (onNavigate) => {
             <img src="">
             <textarea placeholder="Add a comment"></textarea>
         </div>
+      </div>
     </section>
    `;
 
-   const divContainerPost = document.getElementsByClassName("container-addPost__text");
-   divContainerPost.addEventListener("click");
+  $section.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('target:', e.target);
+
+    console.log('id', e.target.getAttribute('id'));
+
+    if (e.target.getAttribute('id') === 'addPost') {
+      Modal('hola modal', 'body');
+    }
+  });
 
   return $section;
 };
-
