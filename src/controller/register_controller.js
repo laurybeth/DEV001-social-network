202dc40f310470a016c022a712bab65b18a8205a
laurybeth <laurybeth@msn.com>
2022-12-13 20:52:06 -0500

    controlador register completado y función currentUser implementada ok

--- src/controller/register_controller.js
+++ src/controller/register_controller.js
@@ -1,22 +1,8 @@
 import { saveCollectionUsersDoc, readCollectionUserDoc } from '../lib_firebase/db.js';
-import { registerFunction } from '../lib_firebase/auth.js';
+import { register } from '../lib_firebase/auth.js';
 
-export const registration = (name, email, birthday, password) => registerFunction(email, password)
-  .then((userCredential) => {
-    saveCollectionUsersDoc(name, email, birthday)
-      .then((docRef) => {
-        readCollectionUserDoc(docRef.id)
-          .then((docSnap) => {
-            console.log(docSnap.data());
-            return docSnap;
-          })
-
-          .catch((error) => error);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        console.log('saveCollectionUsersDoc - The promise no exist', errorCode);
-      });
-
-    const user = userCredential.user;
-  });
+export const registerTasks = (name, email, birthday, password) => register(email, password)
+  .then(() => saveCollectionUsersDoc(name, email, birthday));
+  /* readCollectionUserDoc(docRef.id)
+        .then((docSnap) => {
+          console.log(docSnap.data()); */
