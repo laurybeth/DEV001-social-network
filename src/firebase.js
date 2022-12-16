b6c3b32e90cbb9ea623fb713cc31fe9614c35b63
laurybeth <laurybeth@msn.com>
2022-12-16 14:02:16 -0500

    se agregó referencia a firebase storage en archivo firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
- // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-
+import { getStorage, ref } from 'firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,5 +20,7 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage();
+export const storageRef = ref(storage);
 
-//console.log(app, db, auth);
+// console.log(app, db, auth);
